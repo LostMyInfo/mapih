32e033e4996eb8e538f07560ea8f98a3665c9a0f
Goodsie <gidsola@users.noreply.github.com>
2022-09-17 09:16:05 -0400

    missed api assign

--- api.js
+++ api.js
@@ -5,6 +5,7 @@ const Discord = {
     Channels: require('./api/discord/channels'),
     Guilds: require('./api/discord/guilds'),
     Interactions: require('./api/discord/interactions'),
+    Oauth2: require('./api/discord/oauth2'),
     Users: require('./api/discord/users'),
 
 };
