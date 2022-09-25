89b8ea894737860a1d39a707b668ed4342e38984
Goodsie <gidsola@users.noreply.github.com>
2022-09-24 23:15:58 -0400

    Cleanups, Style, Comments

--- README.md
+++ README.md
@@ -1,17 +1,27 @@
-### Welcome to the Wiki!  
+# Welcome to the Wiki  
 
 Mapih is a collection of Discord Api handlers as well as other useful(I think anyways 😁) Utilities.
 
-### <u>Table of Contents:</u>  
-#### Discord:  
- * ##### [AuditLog](https://github.com/gidsola/mapih/wiki/Auditlog)
- * ##### [Channels](https://github.com/gidsola/mapih/wiki/Channels)
- * ##### [Guilds](https://github.com/gidsola/mapih/wiki/Guilds)
- * ##### [Interactions](https://github.com/gidsola/mapih/wiki/Interactions)
- * ##### [Oauth2](https://github.com/gidsola/mapih/wiki/Oauth2)
- * ##### [Users](https://github.com/gidsola/mapih/wiki/Users)
+## <u>Table of Contents:</u>  
 
-#### Utils:  
- * ##### Https
- * ##### Aray - currently specific to [Z] project
- * ##### Timestamp
+### Discord  
+
+* #### [AuditLog](https://github.com/gidsola/mapih/wiki/Auditlog)
+
+* #### [Channels](https://github.com/gidsola/mapih/wiki/Channels)
+
+* #### [Guilds](https://github.com/gidsola/mapih/wiki/Guilds)
+
+* #### [Interactions](https://github.com/gidsola/mapih/wiki/Interactions)
+
+* #### [Oauth2](https://github.com/gidsola/mapih/wiki/Oauth2)
+
+* #### [Users](https://github.com/gidsola/mapih/wiki/Users)
+
+### Utils  
+
+* #### Https
+
+* #### Aray - currently specific to [Z] project
+
+* #### Timestamp
--- api/discord/users.js
+++ api/discord/users.js
@@ -1,50 +1,63 @@
 const https = require(`../utils/https`);
 
 module.exports = {
-    /* 
-    Get Current User
-    GET /users/@me
-    Returns the user object of the requester's account. For OAuth2, this requires the identify scope, 
-    which will return the object without an email, and optionally the email scope, which returns the object with an email. */
+    /** 
+    Get Current User  
 
-    async getCurrentUser(params) {
-        try{
-            if (attempt = await https.get({
-                url: encodeURI(`discord.com`),
-                path: encodeURI(`/api/users/@me`),
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bot ${process.env.token}`,
-                },
-                body: '',
-            })) return JSON.parse(attempt.body);
+    Endpoint: /users/@me  
+    Returns the user object of the requester's account. For OAuth2, this requires the identify scope.  
+    Returns the user object without an email. Optionally with the email scope, will return an object with an email. */
+    async getCurrentUser() {
+        try {
+            if (
+                (attempt = await https.get({
+                    url: encodeURI(`discord.com`),
+                    path: encodeURI(`/api/users/@me`),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bot ${process.env.token}`,
+                    },
+                    body: '',
+                }))
+            ) return JSON.parse(attempt.body);
             else return false;
-        }catch(e){console.log(e)}
+        } catch (e) {
+            console.log(e)
+        }
     }, //End getCurrentUser
 
-    /*
-    Get User
-    GET /users/{user.id}
+    /**
+    Get User  
+
+    Endpoint: /users/{user.id}  
     Returns a user object for a given user ID. */
     async getUser(params) {
-        if (attempt = await https.get({
-            url: encodeURI(`discord.com`),
-            path: encodeURI(`/api/users/${params.user_id}`),
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bot ${process.env.token}`,
-            },
-            body: '',
-        })) return JSON.parse(attempt.body);
-        else return false;
+        try {
+            if (
+                (attempt = await https.get({
+                    url: encodeURI(`discord.com`),
+                    path: encodeURI(`/api/users/${params.user_id}`),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bot ${process.env.token}`,
+                    },
+                    body: '',
+                }))
+            ) return JSON.parse(attempt.body);
+            else return false;
+        } catch (e) {
+            console.log(e);
+        }
     }, //End getUser
 
-    /* 
-    Modify Current User
-    PATCH /users/@me
-    Modify the requester's user account settings. Returns a user object on success. Fires a User Update Gateway event. */
+    /** 
+    Modify Current User  
+
+    Endpoint: /users/@me  
+    Modify the requester's user account settings.  
+    Returns a user object on success and triggers a User Update Gateway event. */
     async modifyCurrentUser(params) {
-        try{
+        try {
             if (
                 (attempt = await https.patch({
                     url: encodeURI(`discord.com`),
@@ -54,76 +67,96 @@ module.exports = {
                         Authorization: `Bot ${process.env.token}`,
                     },
                     body: JSON.stringify({
-                        "username" : params.username,
-                        "avatar" : params.avatar
+                        username: params.username,
+                        avatar: params.avatar
                     }),
                 }))
             ) return JSON.parse(attempt.body);
-            else return false;  
-        }catch(e){console.log(e)}
+            else return false;
+        } catch (e) {
+            console.log(e)
+        }
     }, // END modifyCurrentUser
 
-    /*
-    Get Current User Guilds
-    GET /users/@me/guilds
+    /**
+    Get Current User Guilds  
+    Endpoint: /users/@me/guilds  
     Returns a list of partial guild objects the current user is a member of. Requires the guilds OAuth2 scope. */
-    async getCurrentUserGuilds(params) {
-        if (attempt = await https.get({
-            url: encodeURI(`discord.com`),
-            path: encodeURI(`/api/users/@me/guilds`),
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bot ${process.env.token}`,
-            },
-            body: '',
-        })) return JSON.parse(attempt.body);
-        else return false;
+    async getCurrentUserGuilds() {
+        try {
+            if (
+                (attempt = await https.get({
+                    url: encodeURI(`discord.com`),
+                    path: encodeURI(`/api/users/@me/guilds`),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bot ${process.env.token}`,
+                    },
+                    body: '',
+                }))
+            ) return JSON.parse(attempt.body);
+            else return false;
+        } catch (e) {
+            console.log(e);
+        }
     }, //End getCurrentUserGuilds
 
-    /*
-    Get Current User Guild Member
-    GET /users/@me/guilds/{guild.id}/member
+    /**
+    Get Current User Guild Member  
+
+    Endpoint: /users/@me/guilds/{guild.id}/member  
     Returns a guild member object for the current user. Requires the guilds.members.read OAuth2 scope. */
     async getCurrentUserGuildMember(params) {
-        if (attempt = await https.get({
-            url: encodeURI(`discord.com`),
-            path: encodeURI(`/api/users/@me/guilds/${params.guild_id}/member`),
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bot ${process.env.token}`,
-            },
-            body: '',
-        })) return JSON.parse(attempt.body);
-        else return false;
+        try {
+            if (
+                (attempt = await https.get({
+                    url: encodeURI(`discord.com`),
+                    path: encodeURI(`/api/users/@me/guilds/${params.guild_id}/member`),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bot ${process.env.token}`,
+                    },
+                    body: '',
+                }))
+            ) return JSON.parse(attempt.body);
+            else return false;
+        } catch (e) {
+            console.log(e);
+        }
     }, //End getCurrentUserGuildMember
 
-    /*
-    Leave Guild
-    DELETE /users/@me/guilds/{guild.id}
+    /**
+    Leave Guild  
+
+    Endpoint: /users/@me/guilds/{guild.id}  
     Leave a guild. Returns a 204 empty response on success. */
-    async leaveGuild(params){
-        try{
-            if (attempt = await https.del({
-                url: encodeURI(`discord.com`),
-                path: encodeURI(`/api/users/@me/guilds/${params.guild_id}`),
-                headers: {
-                    //'Content-Type': 'application/json',
-                    Authorization: `Bot ${process.env.token}`,
-                },
-                body: '',
-            })) return attempt.statusCode == 204;
+    async leaveGuild(params) {
+        try {
+            if (
+                (attempt = await https.del({
+                    url: encodeURI(`discord.com`),
+                    path: encodeURI(`/api/users/@me/guilds/${params.guild_id}`),
+                    headers: {
+                        //'Content-Type': 'application/json',
+                        Authorization: `Bot ${process.env.token}`,
+                    },
+                    body: '',
+                }))
+            ) return attempt.statusCode == 204;
             else return false;
-        }catch(e){console.log(e)}
+        } catch (e) {
+            console.log(e)
+        }
     }, // End leaveGuild
 
-    /*
-    Create DM
-    POST /users/@me/channels
-    Create a new DM channel with a user. Returns a DM channel object.
-    This also sends a message to the user */
+    /**
+    Create DM  
+    
+    Endpoint: /users/@me/channels  
+    Create a new DM channel with a user.  
+    Returns a DM channel object. */
     async createDM(params) {
         try {
-            //let attempt;
             if (
                 (attempt = await https.post({
                     url: encodeURI(`discord.com`),
@@ -138,7 +171,6 @@ module.exports = {
                 }))
             ) {
                 let parsed = JSON.parse(attempt.body);
-                console.log("Parsed Create DM Channel", parsed);
                 if (
                     (dm = await https.post({
                         url: encodeURI(`discord.com`),
@@ -162,23 +194,22 @@ module.exports = {
                         }),
                     }))
                 ) return JSON.parse(dm.body);
-            }
-            else return false;
+            } else return false;
         } catch (e) {
             console.log(e);
         }
     }, // End create DM
 
-    /*
-    Create Group DM
-    POST /users/@me/channels
-    Create a new group DM channel with multiple users. Returns a DM channel object. 
+    /**
+    Create Group DM  
+
+    Endpoint: /users/@me/channels  
+    Create a new group DM channel with multiple users.  
+    Returns a DM channel object.  
     This endpoint was intended to be used with the now-deprecated GameBridge SDK. 
     DMs created with this endpoint will not be shown in the Discord client */
-
     async createGroupDM(params) {
         try {
-            //let attempt;
             if (
                 (attempt = await https.post({
                     url: encodeURI(`discord.com`),
@@ -188,7 +219,7 @@ module.exports = {
                         Authorization: `Bot ${process.env.token}`,
                     },
                     body: JSON.stringify({
-                        access_tokens : params.access_tokens,
+                        access_tokens: params.access_tokens,
                         nicks: params.nicks
                     }),
                 }))
@@ -217,29 +248,33 @@ module.exports = {
                         }),
                     }))
                 ) return JSON.parse(dm.body);
-            }
-            else return false;
+            } else return false;
         } catch (e) {
             console.log(e);
         }
     }, // End createGroupDM
 
-    /*
-    Get User Connections
-    GET /users/@me/connections
+    /**
+    Get User Connections  
+    Endpoint: /users/@me/connections  
     Returns a list of connection objects. Requires the connections OAuth2 scope. */
-    async getUserConnections(params) {
-        if (attempt = await https.get({
-            url: encodeURI(`discord.com`),
-            path: encodeURI(`/api/users/@me/connections`),
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bot ${process.env.token}`,
-            },
-            body: '',
-        })) return JSON.parse(attempt.body);
-        else return false;
+    async getUserConnections() {
+        try {
+            if (
+                (attempt = await https.get({
+                    url: encodeURI(`discord.com`),
+                    path: encodeURI(`/api/users/@me/connections`),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bot ${process.env.token}`,
+                    },
+                    body: '',
+                }))
+            ) return JSON.parse(attempt.body);
+            else return false;
+        } catch (e) {
+            console.log(e);
+        }
     }, //End getUser
-    
 
 }; // End Module Exports
